beab769a1ea66cd8cb7d078ac7273cc5805543ca
Maia <maia.mcc@gmail.com>
2015-03-03 16:53:49 -0500

    preliminary message sending/responding capabilities

--- rsvp_bot.py
+++ rsvp_bot.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
+
 import os
 import zulip
+
+class SendMsgError(Exception): pass
 
 HELP_MSG = """Hi, I'm RSVP Bot! I don't do anything yet. Sorry."""
 
@@ -12,26 +15,76 @@ client = zulip.Client(email=os.environ['RSVPBOT_USERNAME'],
 
 all_events = []
 
-def respond(msg):
-    if msg["type"] == "private":
-        private_msg_respond(msg)
-    elif msg["sender_email"] != "punbot-bot@students.hackerschool.com" and \
-        "@**rsvp bot**" in msg["content"]:
-        # and msg["stream"] in [event.stream for event in all_events]:
-            stream_msg_respond(msg)
+class Event(object):
+    def __init__(self, hosts=[], subject=None, shortname=None, attending=[],
+            description=None, posted=False):
+        self.hosts = hosts
+        self.subject = subject
+        self.shortname = shortname
+        self.attending = attending
+        self.description = description
+        self.posted = posted
 
-def private_msg_respond(msg):
-    print"you sent me a private message"
+def process_incoming_message(msg):
+    if msg["sender_email"] != "rsvp-bot@students.hackerschool.com":
+        if msg["type"] == "private":
+            respond_private_msg(msg)
+        elif "@**rsvp bot**" in msg["content"]:
+            # and msg["stream"] in [event.stream for event in all_events]:
+            respond_stream_msg(msg)
 
-def stream_msg_respond(msg):
-    print "you sent a message to a stream"
+def respond_private_msg(msg):
+    # if lower(msg["content"]) == "help":
+    content = msg["content"]
+    send_response_msg(msg, "you sent me: %s" % content)
+    send_new_msg("private", "and here's a new message!", recipient="maia.mcc@gmail.com")
+
+def respond_stream_msg(msg):
+    content = msg["content"]
+    send_response_msg(msg, "you sent me: %s" % content)
+    send_new_msg("stream", "and here's a new message!", stream="bot-test", subject="stuffz")
+
+def send_response_msg(incoming_msg, outgoing_text):
+    if incoming_msg["type"] == "private":
+        client.send_message({
+            "type": "private",
+            "to": incoming_msg["sender_email"],
+            "content": outgoing_text
+        })
+    elif incoming_msg["type"] == "stream":
+        client.send_message({
+            "type": "stream",
+            "subject": incoming_msg["subject"],
+            "to": incoming_msg['display_recipient'],
+            "content": outgoing_text
+        })
+
+def send_new_msg(msgtype, outgoing_text, recipient=None, stream=None, subject=None):
+    """Sends a Zulip message (NOT in response to a previous message). Specify type as 'private'
+        or 'stream'. If private, 'recipient' will be a single user email, 'stream' and 'subject'
+        will be None. If public, set 'stream' and 'subject', 'recipient' will be None."""
+    if msgtype == "private":
+        client.send_message({
+            "type": "private",
+            "to": recipient,
+            "content": outgoing_text
+        })
+    elif msgtype == "stream":
+        client.send_message({
+            "type": "stream",
+            "to": stream,
+            "subject": subject,
+            "content": outgoing_text
+        })
+    else:
+        raise SendMsgError("Unrecognized message type")
 
 def print_message(msg):
     for k, v in msg.iteritems():
         print k, "-->", v
 
 # This is a blocking call that will run forever
-client.call_on_each_message(respond)
+client.call_on_each_message(process_incoming_message)
 
 """
 An event has:
